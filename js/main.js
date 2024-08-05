38e35b67fd264dbc9e45ac61aa57bf46f36ae553
BrignerCarrielR <brignerarielc@gmail.com>
2024-08-05 11:31:27 -0500

    Version 1

--- js/main.js
+++ js/main.js
@@ -23,17 +23,44 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+function validarYEnviarCorreo() {
+    let nombres = document.getElementById('nombres').value;
+    let correo = document.getElementById('correo').value;
+    let mensaje = document.getElementById('mensaje').value;
 
-function EnviarCorreo() {
-    let nombres = document.getElementById('nombres').value
-    let correo = document.getElementById('correo').value
-    let mensaje = document.getElementById('mensaje').value
-    console.log(nombres, correo, mensaje)
+    let errorMessage = '';
+
+    // Validación de nombre (debe contener al menos un nombre y un apellido)
+    let nombreValido = /^[a-zA-Z]+ [a-zA-Z]+$/.test(nombres);
+    if (!nombreValido) {
+        errorMessage = 'El nombre debe contener al menos un nombre y un apellido.';
+    }
+
+    // Validación de correo (debe ser un correo válido)
+    let correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correo);
+    if (!correoValido) {
+        errorMessage = 'El correo debe ser válido.';
+    }
+
+    // Validación de mensaje (debe tener al menos 5 palabras)
+    let mensajeValido = mensaje.split(' ').filter(word => word.trim().length > 0).length >= 5;
+    if (!mensajeValido) {
+        errorMessage = 'El mensaje debe tener al menos 5 palabras.';
+    }
+
+    if (errorMessage) {
+        document.getElementById('modalErrorMessage').innerText = errorMessage;
+        $('#errorModal').modal('show');
+        return;
+    }
+
+    console.log(nombres, correo, mensaje);
 
     // Envía el correo utilizando EmailJS
     const serviceID = 'service_atvwoci';
     const templateID = 'template_gxjy74d';
     const userID = 'MSBVq_POAKjwB1Ucr';
+
     // Define los datos del correo
     const dataCorreo = {
         nombres: nombres, // Utiliza el nombre del cliente
@@ -45,9 +72,13 @@ function EnviarCorreo() {
     emailjs.send(serviceID, templateID, dataCorreo, userID)
         .then(function (response) {
             console.log('Correo enviado con éxito:', response);
+            $('#successModal').modal('show');
         })
         .catch(function (error) {
             console.error('Error al enviar el correo:', error);
+            document.getElementById('modalErrorMessage').innerText = 'Error al enviar el correo.';
+            $('#errorModal').modal('show');
         });
 }
+
 
